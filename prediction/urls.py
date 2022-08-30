0ee0e18d56b0f34d03b60af114940673fed2531a
darth-anish <anishman99@gmail.com>
2022-08-30 09:26:11 +0545

    Visualizations and statistical testing table added

--- prediction/urls.py
+++ prediction/urls.py
@@ -6,4 +6,5 @@ app_name = 'prediction'
 
 urlpatterns=[
     path('',views.home,name='home'),
+    path('features',views.show_significant_features,name='sig_features')
 ]
--- prediction/views.py
+++ prediction/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'prediction/home.html')
+
+def show_significant_features(request):
+    return render(request, 'prediction/significant_features.html')
